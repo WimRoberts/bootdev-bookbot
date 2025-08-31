787ca42c16baa9ee320cf52835b701f2298a471e
Wim Roberts <jackson@rbrts.co>
2025-08-31 06:38:57 -0700

    added function to stat.py to generate dictionary of word counts

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from stats import get_num_words
+from stats import get_char_count
 
 get_num_words()
+dict=get_char_count()
+print(dict)
--- stats.py
+++ stats.py
@@ -12,3 +12,62 @@ def get_num_words():
     #for word in num_words:
     #    word_count += 1
     print (f"{word_count} words found in the document")
+
+
+
+
+def get_char_count():
+    book_text = get_book_text("./books/frankenstein.txt").lower()
+    char_count = {
+        "a": 0,
+        "b": 0,
+        "c": 0,
+        "d": 0,
+        "e": 0,
+        "f": 0,
+        "g": 0,
+        "h": 0,
+        "i": 0,
+        "j": 0,
+        "k": 0,
+        "l": 0,
+        "m": 0,
+        "n": 0,
+        "o": 0,
+        "p": 0,
+        "q": 0,
+        "r": 0,
+        "s": 0,
+        "t": 0,
+        "u": 0,
+        "v": 0,
+        "w": 0,
+        "x": 0,
+        "y": 0,
+        "z": 0,
+        "1": 0,
+        "2": 0,
+        "3": 0,
+        "4": 0,
+        "5": 0,
+        "6": 0,
+        "7": 0,
+        "8": 0,
+        "9": 0,
+        "0": 0,
+        ",": 0,
+        ".": 0,
+        "!": 0,
+        "?": 0,
+        "'": 0,
+        " ": 0,
+        ";": 0,
+        ":": 0,
+    }
+    for char in book_text:
+        if char in char_count:
+            char_count[char] += 1
+    return(char_count)
+
+
+get_char_count()
