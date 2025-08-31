553d92043907da34ca65f22c3cf165ae77a3dc2f
Wim Roberts <jackson@rbrts.co>
2025-08-31 05:54:57 -0700

    adding a function to count the word in a txt files

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings fellow human")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        # do something with f (the file) here
+        # f is a file object
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book_text = get_book_text("./books/frankenstein.txt")
+    num_words = book_text.split()
+    word_count = len(num_words)
+    #for word in num_words:
+    #    word_count += 1
+    print(f"{word_count} words found in the document")
+
+main()
