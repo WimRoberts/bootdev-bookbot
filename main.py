dba2c0e7acbb8bdeebea076e85eae09eb61ec076
Wim Roberts <jackson@rbrts.co>
2025-09-09 09:10:41 -0700

    sending final commit of the project

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from stats import get_num_words
 from stats import get_char_count
 from stats import sort_char_count
+import sys
 
+#check for proper usage:
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
-char_count = get_char_count()
+char_count = get_char_count(sys.argv[1])
 sorted_char_count = sort_char_count(char_count)
 def main():
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
-    get_num_words()
+    get_num_words(sys.argv[1])
     print("-------- Character Count --------")
     for item in sorted_char_count:
         print(f"{item['char']}: {item['num']}")
